246deab752d344d00a3964d92a896a4173771e4d
DerekTond <strike1894@126.com>
2019-03-12 20:47:27 +0800

    "Orignize application with package"

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 # 注意更新这里的路径，把 app.root_path 添加到 os.path.dirname() 中
 # 以便把文件定位到项目根目录
+prefix = 'sqlite:///'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
